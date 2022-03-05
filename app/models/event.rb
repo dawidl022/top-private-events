2da4060f893273c615fd8128f25292fc7ce059d0
Dawid Lachowicz <domdaw7@gmail.com>
2022-03-05 15:52:31 +0000

    Add attendee list to event show view

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,6 @@ class Event < ApplicationRecord
   validates_presence_of :name, :start_time, :location
 
   belongs_to :creator, class_name: "User"
+  has_many :event_attendances
+  has_many :attendees, through: :event_attendances, class_name: "User"
 end
