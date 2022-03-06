d64d00b9b3937e8d4f43e9f321780e346823809b
Dawid Lachowicz <domdaw7@gmail.com>
2022-03-06 10:42:33 +0000

    Add possibility to edit/delete events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,7 @@
 class EventsController < ApplicationController
+  before_action :require_login, except: [:index, :show]
+  before_action :require_ownership, only: [:edit, :update, :destroy]
+
   def index
     @events = Event
     @filter = event_filter || :all
@@ -17,13 +20,19 @@ class EventsController < ApplicationController
     @event = Event.new
   end
 
-  def create
-    unless user_signed_in?
-      flash[:warning] = "Please sign in to post an event."
-      redirect_to new_user_session_url
-      return
-    end
+  def edit
+  end
 
+  def update
+    if @event.update(event_params)
+      flash[:success] = "Event details updated successfully!"
+      redirect_to current_user
+    else
+      render :edit
+    end
+  end
+
+  def create
     @event = current_user.events.build(event_params)
     if @event.save
       flash[:success] = "Event created successfully!"
@@ -33,7 +42,38 @@ class EventsController < ApplicationController
     end
   end
 
+  def destroy
+    @event.destroy
+    flash[:success] = "Event deleted successfully!"
+    redirect_to current_user
+  end
+
   private
+
+    @@action_display_names = {
+      new: "create", create: "create", edit: "edit", update: "edit",
+      destroy: "delete"
+    }
+
+    def require_login
+      unless user_signed_in?
+        flash[:warning] =
+          "Please sign in to #{@@action_display_names[action_name.to_sym]} events."
+          redirect_to new_user_session_url
+        return
+      end
+    end
+
+    def require_ownership
+      @event = Event.find_by_id(params[:id])
+
+      unless @event.creator == current_user
+        flash[:error] =
+          "You cannot #{@@action_display_names[action_name.to_sym]} " \
+          "events that someone else created"
+        redirect_to root_url
+      end
+    end
 
     def event_params
       params.require(:event).permit(:name, :description, :start_time, :end_time,
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ApplicationRecord
   validates_presence_of :name, :start_time, :end_time ,:location
 
   belongs_to :creator, class_name: "User"
-  has_many :event_attendances
+  has_many :event_attendances, dependent: :delete_all
   has_many :attendees, through: :event_attendances, class_name: "User"
 
   scope :upcoming, -> do
