d90017acc59ab46ffc4652cd988d2a051655cc0a
Dawid Lachowicz <domdaw7@gmail.com>
2022-03-06 11:19:03 +0000

    Add custom event date validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,14 @@
+class DateValidator < ActiveModel::Validator
+  def validate(event)
+    unless event.end_time.blank? || event.end_time >= event.start_time
+      event.errors.add :end_time, "cannot be before start time"
+    end
+  end
+end
+
 class Event < ApplicationRecord
   validates_presence_of :name, :start_time, :end_time ,:location
+  validates_with DateValidator
 
   belongs_to :creator, class_name: "User"
   has_many :event_attendances, dependent: :delete_all
